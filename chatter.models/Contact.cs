2ac488fa898b0c3b4e9038086a62c977fe5817eb
Cameron Presley <Cameron@TheSoftwareMentor.com>
2021-10-09 11:47:43 -0400

    Added rough shape of services

---
+++ chatter.models/Contact.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace chatter.models
+{
+    public record Contact (string FirstName, string LastName, string ContactInfo);
+}
--- chatter/Program.cs
+++ chatter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using chatter.models;
 
 namespace chatter
 {
@@ -6,6 +9,16 @@ namespace chatter
     {
         static void Main(string[] args)
         {
+            var moduleLoader = new ModuleLoader();
+            var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var modules = moduleLoader.Load(directory);
+            if (modules.Any())
+            {
+                Console.WriteLine("There are modules!");
+            }
+            else {
+                Console.WriteLine("Nothing to see here!");
+            }
             Console.WriteLine("Hello World!");
         }
     }
